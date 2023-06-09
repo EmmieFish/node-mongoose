2af0176f4cc81085023213b0fb02e1ecc4151b5e
EmmieFish <105821167+EmmieFish@users.noreply.github.com>
2023-06-09 16:43:36 -0700

    broken code but will look later

--- index.js
+++ index.js
@@ -1,33 +1,52 @@
-const mongoose = require('mongoose')
-const Campsite = require('./models/campsite')
+const mongoose = require("mongoose");
+const Campsite = require("./models/campsite");
 
-const url = 'mongodb://localhost:27017/nucampsite'
+const url = "mongodb://localhost:27017/nucampsite";
 const connect = mongoose.connect(url, {
     useCreateIndex: true,
+    useFindAndModify: false,
     useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+    useUnifiedTopology: true,
+});
 
 connect.then(() => {
-    console.log('Connected correctly to server')
+    console.log("Connected correctly to server");
 
     Campsite.create({
-        name: 'React Lake Campground',
-        description: 'testhello'
+        name: "React Lake Campground2",
+        description: "testhello",
     })
-    .then(campsite => {
-        console.log(campsite)
-        return Campsite.find()
-    })
-    .then(campsites => {
-        console.log(campsites)
-        return Campsite.deleteMany()
-    })
-    .then(() => {
-        return mongoose.connection.close()
-    })
-    .catch(err => {
-        console.log(err)
-        mongoose.connection.close()
-    })
-})
+        .then((campsite) => {
+            console.log(campsite);
+
+            return Campsite.findByIdAndUpdate(
+                campsite._id,
+                {
+                    $set: { description: "Updated Test Document" },
+                },
+                { new: true }
+            );
+        })
+        .then((campsite) => {
+            console.log(campsite);
+
+            campsite.comments.push({
+                rating: 2,
+                text: "What a shitty view!",
+                author: "Billy Boi",
+            });
+
+            return campsite.save();
+        })
+        .then((campsite) => {
+            console.log(campsite);
+            return Campsite.deleteMany();
+        })
+        .then(() => {
+            return mongoose.connection.close();
+        })
+        .catch((err) => {
+            console.log(err);
+            mongoose.connection.close();
+        });
+});
